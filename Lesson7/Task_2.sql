96e187f50c4284c2cc7b323e782f1f4c488d5294
Игнат Александр <ignataf@mosreg.ru>
2020-08-06 18:20:36 +0300

    Урок 7, 1-3 задания + скрипт таблиц shop

---
+++ Lesson7/Task_2.sql
@@ -0,0 +1,4 @@
+﻿-- Выведите список товаров products и разделов catalogs, который соответствует товару.
+
+SELECT p.name AS 'Товар', c.name AS 'Категория' FROM products p
+LEFT JOIN catalogs c ON p.catalog_id = c.id;
---
+++ Lesson7/Task_3.sql
@@ -0,0 +1,38 @@
+﻿-- Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
+
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+  id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `from` varchar(255) COMMENT 'Город вылета',
+  `to` varchar(255) COMMENT 'Город назначения'
+);
+
+INSERT INTO flights
+  VALUES (NULL, 'moscow', 'omsk'),
+  (NULL, 'novgorod', 'kazan'),
+  (NULL, 'irkutsk', 'moscow'),
+  (NULL, 'omsk', 'irkutsk'),
+  (NULL, 'moscow', 'kazan');
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+  label varchar(255) COMMENT 'Ключ города',
+  name varchar(255) COMMENT 'Наименование города'
+);
+
+INSERT INTO cities
+  VALUES ('moscow', 'Москва'),
+  ('irkutsk', 'Иркутск'),
+  ('novgorod', 'Новгород'),
+  ('kazan', 'Казань'),
+  ('omsk', 'Омск');
+
+
+SELECT
+  CONCAT(fc.name," (",f.`from`,")") AS 'from',
+  CONCAT(tc.name," (",f.`to`,")") AS 'to'  
+FROM flights f
+  JOIN cities fc
+    ON fc.label = f.`from`
+  JOIN cities tc
+    ON tc.label = f.`to`
