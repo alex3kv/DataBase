1dae53256ca782a66ce3ff3786376a8acb748607
Александр Игнат <alex3kv@gmail.com>
2020-08-18 10:58:07 +0300

    Урок 10, задания 1-2

---
+++ Lesson10/Task_1.sql
@@ -0,0 +1,10 @@
+﻿-- 1. Проанализировать какие запросы могут выполняться наиболее часто в процессе работы приложения и добавить необходимые индексы.
+
+-- будет поиск по имени и фамилии
+CREATE INDEX users_first_name_idx ON users(first_name);
+CREATE INDEX users_last_name_idx ON users(last_name);
+
+-- поиск по дню рождения, городу и стране
+CREATE INDEX profiles_birthday_idx ON profiles(birthday);
+CREATE INDEX profiles_city_idx ON profiles(city);
+CREATE INDEX profiles_country_idx ON profiles(country);
---
+++ Lesson10/Task_2.sql
@@ -0,0 +1,27 @@
+﻿-- 2. Задание на оконные функции
+-- Построить запрос, который будет выводить следующие столбцы:
+-- имя группы
+-- среднее количество пользователей в группах
+-- самый молодой пользователь в группе
+-- самый старший пользователь в группе
+-- общее количество пользователей в группе
+-- всего пользователей в системе
+-- отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
+
+SELECT DISTINCT
+  c.name AS 'Группа',
+  COUNT(cu.user_id) OVER () / COUNT(c.id) OVER () AS 'Среднее',
+  FIRST_VALUE(CONCAT(u.first_name, ' ', u.last_name, ' (', p.birthday, ')')) OVER w AS 'Младший',
+  LAST_VALUE(CONCAT(u.first_name, ' ', u.last_name, ' (', p.birthday, ')')) OVER w AS 'Старший',  
+  COUNT(cu.user_id) OVER w AS 'Общее',
+  COUNT(u.id) OVER () AS 'Всего',
+  COUNT(cu.user_id) OVER w / COUNT(u.id) OVER () * 100 AS 'Отношение'
+FROM (users u
+  JOIN profiles p
+    ON u.id = p.user_id
+  LEFT JOIN communities_users cu
+    ON u.id = cu.user_id
+  LEFT JOIN communities c
+    ON cu.community_id = c.id)
+WINDOW w AS (PARTITION BY c.id
+ORDER BY p.birthday DESC RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING);
